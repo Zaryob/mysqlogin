c4eb2e4e8d85cbadcec68467791577769e8a329c
Suleyman Poyraz <zaryob.dev@gmail.com>
2021-04-24 16:44:28 +0300

    Design made.

--- lib/login.dart
+++ lib/login.dart
@@ -1,1 +1,90 @@
+import 'package:flutter/material.dart';
 
+class LoginPage extends StatefulWidget {
+  LoginPage({Key? key}) : super(key: key);
+
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+
+    return Scaffold(
+      body: Container(
+        padding: EdgeInsets.symmetric(
+            vertical: size.height * 0.2, horizontal: size.width * 0.2),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Text(
+                'Login',
+                style: Theme.of(context).textTheme.headline4,
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Text(
+                'Please fill in your credentials to login',
+              ),
+              SizedBox(
+                height: 15,
+              ),
+              TextField(
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'User',
+                  hintText: 'Enter your user name.',
+                ),
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              TextField(
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'Password',
+                  hintText: 'Enter your password.',
+                ),
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Container(
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(5),
+                  // ignore: deprecated_member_use
+                  child: FlatButton(
+                    padding: EdgeInsets.symmetric(vertical: 20, horizontal: 40),
+                    color: Colors.blue,
+                    onPressed: () {
+                      /*
+                      Navigator.push(
+                        context,
+                        
+                        MaterialPageRoute(
+                          builder: (context) {
+                            return LoginScreen();
+                          },
+                        ),
+                      );*/
+                    },
+                    child: Text(
+                      "Login",
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold, color: Colors.white),
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
