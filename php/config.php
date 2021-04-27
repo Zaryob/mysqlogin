b7a743ce87b80bd9fa6ad15671c3ba3df7bbc30d
Suleyman Poyraz <zaryob.dev@gmail.com>
2021-04-28 02:29:58 +0300

    Config file generated.

---
+++ php/config.php
@@ -0,0 +1,9 @@
+<?php
+   $link = "http://localhost/"; // Your web  server link. If it is localhost, not change
+   $host = '127.0.0.1';  // Your mysql server ip, If it is localhost use 127.0.0.1, else  change with your one
+   $database = 'users'; // Your database in your mysql else dont change if you wont know
+   $table = 'users'; // Your table in your mysql  else dont change if you wont know
+   $username = 'user'; // Your Database user name. 
+   $password = 'password';// Your Database user s password
+?>
+
